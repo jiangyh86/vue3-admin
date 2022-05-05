04775eddbb66c95a6be96dad2d60fbd411d1912e
jiangyh86 <www.1966283140@qq.com>
2022-05-05 21:39:43 +0800

    feat: 自定义主题变更

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,7 @@
 import variables from '@/styles/variables.scss'
+import { MAIN_COLOR } from '@/constant'
+import { getItem } from '@/utils/storage'
+import { generateColors } from '@/utils/theme'
 
 const getters = {
   token: (state) => state.user.token,
@@ -8,7 +11,12 @@ const getters = {
   hasUserInfo: (state) => {
     return JSON.stringify(state.user.userInfo) !== '{}'
   },
-  cssVar: () => variables,
+  cssVar: state => {
+    return {
+      ...variables,
+      ...generateColors(getItem(MAIN_COLOR))
+    }
+  },
   sidebarOpened: (state) => state.app.sidebarOpened,
   mainColor: state => state.theme.mainColor
 }
